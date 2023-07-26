c1ed776c729c4bddf17ed74a8482a04e68edf7cf
Salem Ashraf <salemashraf2002@gmail.com>
2023-07-26 23:36:50 +0300

    new

--- lib/Screens/MeetPage.dart
+++ lib/Screens/MeetPage.dart
@@ -1,17 +1,24 @@
-// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, file_names
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:meetu/Resources/GoogleAuth.dart';
 import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
 
-class MettingPage extends StatelessWidget {
+class MettingPage extends StatefulWidget {
   final String roomId;
 
   MettingPage({required this.roomId});
 
+  @override
+  State<MettingPage> createState() => _MettingPageState();
+}
+
+class _MettingPageState extends State<MettingPage> {
   final int appId = int.parse(dotenv.get('ZEGO_APP_ID'));
+
   final String appSign = dotenv.get('ZEGO_APP_SIGN');
+
   final GoogleAuth googleAuth = GoogleAuth();
 
   @override
@@ -24,8 +31,14 @@ class MettingPage extends StatelessWidget {
             appSign, // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
         userID: googleAuth.user.uid,
         userName: googleAuth.user.displayName ?? '',
-        conferenceID: roomId,
-        config: ZegoUIKitPrebuiltVideoConferenceConfig(),
+        conferenceID: widget.roomId,
+        config: ZegoUIKitPrebuiltVideoConferenceConfig(
+            turnOnCameraWhenJoining: false,
+            turnOnMicrophoneWhenJoining: false,
+            topMenuBarConfig: ZegoTopMenuBarConfig(
+              style: ZegoMenuBarStyle.light,
+              title: 'MeetU is here',
+            )),
       ),
     );
   }
